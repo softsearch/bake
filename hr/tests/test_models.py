6ca69ebc4dde3d7db41bc3ba87c62850693d976d
warukira <dwarukira@gmail.com>
2019-06-01 13:11:02 +0300

    add

--- hr/tests/test_models.py
+++ hr/tests/test_models.py
@@ -2,16 +2,16 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 from ..models import Employee, Children
 
+
 class EmployeeTest(TestCase):
     def setUp(self):
-        user = get_user_model().objects.create(username = "Test User", password="Password")
-        self.employee = Employee.objects.create(user = user)
+        user = get_user_model().objects.create(username="Test User", password="Password")
+        self.employee = Employee.objects.create(user=user)
 
     def test_create_an_employee_from_user(self):
         employee = Employee.objects.first()
         self.assertEqual(employee.user, get_user_model().objects.first())
 
-    
     def test_create_an_employee_can_have_children(self):
         self.employee.children.create(name="Text")
         self.assertEqual(len(self.employee.children.all()), 1)
